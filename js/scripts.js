ff850eb833a0893d00b5c0a539daee13734476d7
Tomasz <estewez@gmail.com>
2017-12-02 14:35:45 +0100

    repository started

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+var a = prompt('Declare the base of triangle');
+var h = prompt('Declere the height of triangle');
+var triangleArea = a*h/2;
+console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
