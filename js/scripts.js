d5f7b8cfc4e07fd600aa35ffd102bcb3908c5155
Tomasz <estewez@gmail.com>
2017-12-02 14:41:07 +0100

    task completed

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,10 @@
-var a = prompt('Declare the base of triangle');
-var h = prompt('Declere the height of triangle');
-var triangleArea = a*h/2;
-console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+var a = 1;
+var b = 1;
+var value = (a * a) - (2 * a * b) + (b * b);
+if (value > 0) {
+    console.log('wynik dodatni');
+} else if (value == 0) {
+    console.log('wynik równy zero');
+} else {
+    console.log('wynik ujemny');
+}
